71c491005a2e2d38749353b04ff4d83f5b49b555
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-09 00:35:15 +0200

    Add files via upload

---
+++ app/rpc/rpccontext/log.go
@@ -0,0 +1,7 @@
+package rpccontext
+
+import (
+	"github.com/gordanet/gord/infrastructure/logger"
+)
+
+var log = logger.RegisterSubSystem("RPCS")
---
+++ app/rpc/rpchandlers/get_coin_supply.go
@@ -0,0 +1,29 @@
+package rpchandlers
+
+import (
+	"github.com/gordanet/gord/app/appmessage"
+	"github.com/gordanet/gord/app/rpc/rpccontext"
+	"github.com/gordanet/gord/domain/consensus/utils/constants"
+	"github.com/gordanet/gord/infrastructure/network/netadapter/router"
+)
+
+// HandleGetCoinSupply handles the respectively named RPC command
+func HandleGetCoinSupply(context *rpccontext.Context, _ *router.Router, _ appmessage.Message) (appmessage.Message, error) {
+	if !context.Config.UTXOIndex {
+		errorMessage := &appmessage.GetCoinSupplyResponseMessage{}
+		errorMessage.Error = appmessage.RPCErrorf("Method unavailable when kaspad is run without --utxoindex")
+		return errorMessage, nil
+	}
+
+	circulatingSompiSupply, err := context.UTXOIndex.GetCirculatingSompiSupply()
+	if err != nil {
+		return nil, err
+	}
+
+	response := appmessage.NewGetCoinSupplyResponseMessage(
+		constants.MaxSompi,
+		circulatingSompiSupply,
+	)
+
+	return response, nil
+}
