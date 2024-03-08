c77e09d4e457613d440b3e2cec6eecb72fafb3da
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-09 01:59:20 +0200

    Add files via upload

---
+++ util/locks/log.go
@@ -0,0 +1,7 @@
+package locks
+
+import (
+	"github.com/gordanet/gord/infrastructure/logger"
+)
+
+var log = logger.RegisterSubSystem("UTIL")
---
+++ util/staging/log.go
@@ -0,0 +1,7 @@
+package staging
+
+import (
+	"github.com/gordanet/gord/infrastructure/logger"
+)
+
+var utilLog = logger.RegisterSubSystem("UTIL")
