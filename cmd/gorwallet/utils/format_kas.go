3440a337a8fea695f844fdb4b07d4deac008c94c
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-11 15:19:49 +0700

    Update format_kas.go

--- cmd/gorwallet/utils/format_kas.go
+++ cmd/gorwallet/utils/format_kas.go
@@ -7,10 +7,10 @@ import (
 )
 
 // FormatKas takes the amount of sompis as uint64, and returns amount of KAS with 8  decimal places
-func FormatKas(amount uint64) string {
+func FormatGor(amount uint64) string {
 	res := "                   "
 	if amount > 0 {
-		res = fmt.Sprintf("%19.8f", float64(amount)/constants.SompiPerKaspa)
+		res = fmt.Sprintf("%19.8f", float64(amount)/constants.SompiPerGor)
 	}
 	return res
 }
