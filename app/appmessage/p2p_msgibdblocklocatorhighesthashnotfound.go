be4f30fe873f3dcbabf1da466623730c6e1808a9
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-12 19:42:31 +0700

    Update p2p_msgibdblocklocatorhighesthashnotfound.go

--- app/appmessage/p2p_msgibdblocklocatorhighesthashnotfound.go
+++ app/appmessage/p2p_msgibdblocklocatorhighesthashnotfound.go
@@ -1,6 +1,6 @@
 package appmessage
 
-// MsgIBDBlockLocatorHighestHashNotFound represents a kaspa BlockLocatorHighestHashNotFound message
+// MsgIBDBlockLocatorHighestHashNotFound represents a gord BlockLocatorHighestHashNotFound message
 type MsgIBDBlockLocatorHighestHashNotFound struct {
 	baseMessage
 }
