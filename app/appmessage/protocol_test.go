c7f29a640fce61ee71376149c80e16c47e797151
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-12 20:01:54 +0700

    Update protocol_test.go

--- app/appmessage/protocol_test.go
+++ app/appmessage/protocol_test.go
@@ -33,16 +33,16 @@ func TestServiceFlagStringer(t *testing.T) {
 	}
 }
 
-// TestKaspaNetStringer tests the stringized output for kaspa net types.
-func TestKaspaNetStringer(t *testing.T) {
+// TestKaspaNetStringer tests the stringized output for gord net types.
+func TestGordNetStringer(t *testing.T) {
 	tests := []struct {
-		in   KaspaNet
+		in   GordNet
 		want string
 	}{
 		{Mainnet, "Mainnet"},
 		{Testnet, "Testnet"},
 		{Simnet, "Simnet"},
-		{0xffffffff, "Unknown KaspaNet (4294967295)"},
+		{0xffffffff, "Unknown GordNet (4294967295)"},
 	}
 
 	t.Logf("Running %d tests", len(tests))
