cb8f0e2338e904a741ee657cc2758d6fae3a35d2
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-12 19:23:56 +0700

    Update doc.go

--- doc.go
+++ doc.go
@@ -6,7 +6,7 @@ Copyright (c) 2013-2014 Conformal Systems LLC.
 Use of this source code is governed by an ISC
 license that can be found in the LICENSE file.
 
-Kaspad is a full-node kaspa implementation written in Go.
+Gord is a full-node gord implementation written in Go.
 
 The default options are sane for most users. This means gord will work 'out of
 the box' for most users. However, there are also a wide variety of flags that
@@ -21,9 +21,9 @@ For an up-to-date help message:
 	gord --help
 
 The long form of all option flags (except -C) can be specified in a configuration
-file that is automatically parsed when kaspad starts up. By default, the
-configuration file is located at ~/.gor/kaspad.conf on POSIX-style operating
-systems and %LOCALAPPDATA%\kaspad\kaspad.conf on Windows. The -C (--configfile)
+file that is automatically parsed when gord starts up. By default, the
+configuration file is located at ~/.gor/gord.conf on POSIX-style operating
+systems and %LOCALAPPDATA%\gord\gord.conf on Windows. The -C (--configfile)
 flag can be used to override this location.
 */
 package main
