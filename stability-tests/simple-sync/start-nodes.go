e584f79fe906adf50d288c0272892af3c6e50cdc
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 13:08:55 +0700

    Update start-nodes.go

--- stability-tests/simple-sync/start-nodes.go
+++ stability-tests/simple-sync/start-nodes.go
@@ -24,20 +24,20 @@ func startNodes() (teardown func(), err error) {
 	)
 
 	log.Infof("Starting nodes")
-	syncerDataDir, err := common.TempDir("kaspad-datadir-syncer")
+	syncerDataDir, err := common.TempDir("gord-datadir-syncer")
 	if err != nil {
 		panic(errors.Wrapf(err, "error in Tempdir"))
 	}
 	log.Infof("SYNCER datadir: %s", syncerDataDir)
 
-	syncedDataDir, err := common.TempDir("kaspad-datadir-synced")
+	syncedDataDir, err := common.TempDir("gord-datadir-synced")
 	if err != nil {
 		panic(errors.Wrapf(err, "error in Tempdir"))
 	}
 	log.Infof("SYNCED datadir: %s", syncedDataDir)
 
-	syncerCmd, err := common.StartCmd("KASPAD-SYNCER",
-		"kaspad",
+	syncerCmd, err := common.StartCmd("GORD-SYNCER",
+		"gord",
 		common.NetworkCliArgumentFromNetParams(activeConfig().NetParams()),
 		"--appdir", syncerDataDir,
 		"--logdir", syncerDataDir,
@@ -50,8 +50,8 @@ func startNodes() (teardown func(), err error) {
 		return nil, err
 	}
 
-	syncedCmd, err := common.StartCmd("KASPAD-SYNCED",
-		"kaspad",
+	syncedCmd, err := common.StartCmd("GORD-SYNCED",
+		"gord",
 		common.NetworkCliArgumentFromNetParams(activeConfig().NetParams()),
 		"--appdir", syncedDataDir,
 		"--logdir", syncedDataDir,
