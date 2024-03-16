13ac7cc2820336057e55c95de14c3cd0f75078a9
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:37:12 +0700

    Update p2p_ready.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_ready.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_ready.go
@@ -5,13 +5,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_Ready) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_Ready) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrapf(errorNil, "KaspadMessage_Ready is nil")
+		return nil, errors.Wrapf(errorNil, "GordMessage_Ready is nil")
 	}
 	return &appmessage.MsgReady{}, nil
 }
 
-func (x *KaspadMessage_Ready) fromAppMessage(_ *appmessage.MsgReady) error {
+func (x *GordMessage_Ready) fromAppMessage(_ *appmessage.MsgReady) error {
 	return nil
 }
