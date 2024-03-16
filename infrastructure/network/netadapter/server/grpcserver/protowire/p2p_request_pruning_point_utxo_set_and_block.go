8f8d828c1328cbe16ef43e2eba8b5390744c094a
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:43:14 +0700

    Update p2p_request_pruning_point_utxo_set_and_block.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_request_pruning_point_utxo_set_and_block.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_request_pruning_point_utxo_set_and_block.go
@@ -5,9 +5,9 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_RequestPruningPointUTXOSet) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_RequestPruningPointUTXOSet) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrapf(errorNil, "KaspadMessage_RequestPruningPointUTXOSet is nil")
+		return nil, errors.Wrapf(errorNil, "GordMessage_RequestPruningPointUTXOSet is nil")
 	}
 	return x.RequestPruningPointUTXOSet.toAppMessage()
 }
@@ -23,7 +23,7 @@ func (x *RequestPruningPointUTXOSetMessage) toAppMessage() (appmessage.Message, 
 	return &appmessage.MsgRequestPruningPointUTXOSet{PruningPointHash: pruningPointHash}, nil
 }
 
-func (x *KaspadMessage_RequestPruningPointUTXOSet) fromAppMessage(
+func (x *GordMessage_RequestPruningPointUTXOSet) fromAppMessage(
 	msgRequestPruningPointUTXOSet *appmessage.MsgRequestPruningPointUTXOSet) error {
 
 	x.RequestPruningPointUTXOSet = &RequestPruningPointUTXOSetMessage{}
