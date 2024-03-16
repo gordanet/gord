57228cfebb5e30442769e0d49df33216901b7bff
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 11:42:52 +0700

    Update keypair.go

--- cmd/gorwallet/libgorwallet/keypair.go
+++ cmd/gorwallet/libgorwallet/keypair.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/kaspanet/go-secp256k1"
+	"github.com/gordanet/go-secp256k1"
 	"github.com/gordanet/gord/cmd/gorwallet/libgorwallet/bip32"
 	"github.com/gordanet/gord/domain/dagconfig"
 	"github.com/gordanet/gord/util"
