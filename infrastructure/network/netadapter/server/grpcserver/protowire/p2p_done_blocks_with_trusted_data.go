6f5b229254d5fc5aa906629357b2c56e82b735e1
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:28:55 +0700

    Update p2p_done_blocks_with_trusted_data.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_done_blocks_with_trusted_data.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_done_blocks_with_trusted_data.go
@@ -5,13 +5,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_DoneBlocksWithTrustedData) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_DoneBlocksWithTrustedData) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrapf(errorNil, "KaspadMessage_DoneBlocksWithTrustedData is nil")
+		return nil, errors.Wrapf(errorNil, "GordMessage_DoneBlocksWithTrustedData is nil")
 	}
 	return &appmessage.MsgDoneBlocksWithTrustedData{}, nil
 }
 
-func (x *KaspadMessage_DoneBlocksWithTrustedData) fromAppMessage(_ *appmessage.MsgDoneBlocksWithTrustedData) error {
+func (x *GordMessage_DoneBlocksWithTrustedData) fromAppMessage(_ *appmessage.MsgDoneBlocksWithTrustedData) error {
 	return nil
 }
