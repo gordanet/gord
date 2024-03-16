7a48fab56e4b83ac3be70034f85709cfc81512f4
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:46:49 +0700

    Update p2p_transaction_not_found.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_transaction_not_found.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_transaction_not_found.go
@@ -5,9 +5,9 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_TransactionNotFound) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_TransactionNotFound) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrapf(errorNil, "KaspadMessage_TransactionNotFound is nil")
+		return nil, errors.Wrapf(errorNil, "GordMessage_TransactionNotFound is nil")
 	}
 	return x.TransactionNotFound.toAppMessage()
 }
@@ -23,7 +23,7 @@ func (x *TransactionNotFoundMessage) toAppMessage() (appmessage.Message, error) 
 	return appmessage.NewMsgTransactionNotFound(id), nil
 }
 
-func (x *KaspadMessage_TransactionNotFound) fromAppMessage(msgTransactionsNotFound *appmessage.MsgTransactionNotFound) error {
+func (x *GorMessage_TransactionNotFound) fromAppMessage(msgTransactionsNotFound *appmessage.MsgTransactionNotFound) error {
 	x.TransactionNotFound = &TransactionNotFoundMessage{
 		Id: domainTransactionIDToProto(msgTransactionsNotFound.ID),
 	}
