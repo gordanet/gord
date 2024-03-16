76f71607724b82e41922891e6b757fc6363ac19c
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 12:50:58 +0700

    Update transactions.go

--- stability-tests/mempool-limits/transactions.go
+++ stability-tests/mempool-limits/transactions.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/kaspanet/go-secp256k1"
+	"github.com/gordanet/go-secp256k1"
 	"github.com/gordanet/gord/app/appmessage"
 	"github.com/gordanet/gord/domain/consensus/model/externalapi"
 	"github.com/gordanet/gord/domain/consensus/utils/consensushashing"
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	payAddress                       = "kaspasim:qzuax2jhawd354e54thhpd9m9wg03pdzwjlpr4vtq3k7xrpumhhtwa2hkr3ep"
+	payAddress                       = "gorim:qzuax2jhawd354e54thhpd9m9wg03pdzwjlpr4vtq3k7xrpumhhtwa2hkr3ep"
 	payAddressPrivateKey             = "05d8f681e954a550395ee2297fc1a14f6e801f554c0b9d48cd7165a7ea72ff77"
 	fundingCoinbaseTransactionAmount = 1000
 	outputsPerTransaction            = 3
