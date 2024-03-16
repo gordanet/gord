32562d9d2f623d2b7550def16b0c6d7a56657025
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:48:11 +0700

    Update p2p_verack.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_verack.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_verack.go
@@ -5,13 +5,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_Verack) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_Verack) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrapf(errorNil, "KaspadMessage_Verack is nil")
+		return nil, errors.Wrapf(errorNil, "GordMessage_Verack is nil")
 	}
 	return &appmessage.MsgVerAck{}, nil
 }
 
-func (x *KaspadMessage_Verack) fromAppMessage(_ *appmessage.MsgVerAck) error {
+func (x *GordMessage_Verack) fromAppMessage(_ *appmessage.MsgVerAck) error {
 	return nil
 }
