fbbb25f2dcb54051806818753e79e7271dc1f5a7
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 20:23:13 +0700

    Update p2p_addresses.go

--- infrastructure/network/netadapter/server/grpcserver/protowire/p2p_addresses.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/p2p_addresses.go
@@ -5,9 +5,9 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_Addresses) toAppMessage() (appmessage.Message, error) {
+func (x *GordMessage_Addresses) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
-		return nil, errors.Wrap(errorNil, "KaspadMessage_Addresses is nil")
+		return nil, errors.Wrap(errorNil, "GordMessage_Addresses is nil")
 	}
 	addressList, err := x.Addresses.toAppMessage()
 	if err != nil {
