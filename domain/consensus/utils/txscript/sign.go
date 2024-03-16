c4f04584a96d42e504771243a65a0d6e450f1ce8
alexhoong <49112849+alexhoongming@users.noreply.github.com>
2024-03-16 22:48:30 +0700

    Update sign.go

--- domain/consensus/utils/txscript/sign.go
+++ domain/consensus/utils/txscript/sign.go
@@ -5,7 +5,9 @@
 package txscript
 
 import (
-	"github.com/kaspanet/go-secp256k1"
+	"github.com/gordanet/go-secp256k1"
+	 "github.com/gordanet/gord-muhash@v0.0.4"
+	
 	"github.com/gordanet/gord/domain/consensus/model/externalapi"
 	"github.com/gordanet/gord/domain/consensus/utils/consensushashing"
 	"github.com/pkg/errors"
