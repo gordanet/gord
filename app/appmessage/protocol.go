2f2991e76c4cdbcfd7282515e0b706aa84b5f616
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-19 13:44:52 +0200

    Update protocol.go

--- app/appmessage/protocol.go
+++ app/appmessage/protocol.go
@@ -89,7 +89,7 @@ func (f ServiceFlag) String() string {
 	return s
 }
 
-// GordNet represents which gord network a message belongs to.
+// GorNet represents which gor network a message belongs to.
 type KaspaNet uint32
 
 // Constants used to indicate the message gord network. They can also be
