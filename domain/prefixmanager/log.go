58183960f2920eee1d94c66b4e0925755de56be5
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-19 18:54:36 +0200

    Delete domain/prefixmanager directory

--- domain/prefixmanager/log.go
+++
@@ -1,9 +0,0 @@
-package prefixmanager
-
-import (
-	"github.com/gordanet/gord/infrastructure/logger"
-	"github.com/gordanet/gord/util/panics"
-)
-
-var log = logger.RegisterSubSystem("PRFX")
-var spawn = panics.GoroutineWrapperFunc(log)
