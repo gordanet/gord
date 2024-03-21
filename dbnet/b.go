06b3023d97880fb4338cff40a2e86591b56ad856
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-21 19:50:23 +0200

    Update b.go

--- dbnet/b.go
+++ dbnet/b.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/kaspanet/kaspad/blockdag"
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/infrastructure/logger"
-	"github.com/kaspanet/kaspad/stability-tests/common"
+	
+	"github.com/gordanet/gord/domain/consensus/model/externalapi"
+	"github.com/gordanet/gord/infrastructure/logger"
+	"github.com/gordanet/gord/stability-tests/common"
 	"os"
 )
 
