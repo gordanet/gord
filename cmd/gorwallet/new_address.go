7e048aea3a06aed404d924759d741de9bf220983
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-21 14:07:44 +0200

    Add files via upload

---
+++ cmd/gorwallet/new_address.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gordanet/gord/cmd/gorwallet/daemon/client"
+	"github.com/gordanet/gord/cmd/gorwallet/daemon/pb"
+)
+
+func newAddress(conf *newAddressConfig) error {
+	daemonClient, tearDown, err := client.Connect(conf.DaemonAddress)
+	if err != nil {
+		return err
+	}
+	defer tearDown()
+
+	ctx, cancel := context.WithTimeout(context.Background(), daemonTimeout)
+	defer cancel()
+
+	response, err := daemonClient.NewAddress(ctx, &pb.NewAddressRequest{})
+	if err != nil {
+		return err
+	}
+//my-add New address:\n%s\n
+	fmt.Printf("%s", response.Address)
+	return nil
+}
