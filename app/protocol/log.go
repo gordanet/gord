5f18711769bf5201b67854648c9739bb0776e476
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-21 15:29:26 +0200

    Add files via upload

---
+++ app/protocol/log.go
@@ -0,0 +1,9 @@
+package protocol
+
+import (
+	"github.com/gordanet/gord/infrastructure/logger"
+	"github.com/gordanet/gord/util/panics"
+)
+
+var log = logger.RegisterSubSystem("PROT")
+var spawn = panics.GoroutineWrapperFunc(log)
---
+++ app/protocol/peer/log.go
@@ -0,0 +1,7 @@
+package peer
+
+import (
+	"github.com/gordanet/gord/infrastructure/logger"
+)
+
+var log = logger.RegisterSubSystem("PROT")
