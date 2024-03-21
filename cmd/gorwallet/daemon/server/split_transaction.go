85b09d14d69c87cd1329fbbe042375705b5da4cc
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-21 15:45:52 +0200

    Update split_transaction.go

--- cmd/gorwallet/daemon/server/split_transaction.go
+++ cmd/gorwallet/daemon/server/split_transaction.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"github.com/gornet/go-secp256k1"
+	"github.com/kaspanet/go-secp256k1"
 	"github.com/pkg/errors"
 
 	"github.com/gordanet/gord/cmd/gorwallet/libgorwallet"
