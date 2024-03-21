55c00e1be1ec3e0139e92dad69558858cb6c4b9c
gordanet <162464057+gordanet@users.noreply.github.com>
2024-03-21 14:05:50 +0200

    Add files via upload

---
+++ app/appmessage/rpc/rpchandlers/notify_virtual_daa_score_changed.go
@@ -0,0 +1,19 @@
+package rpchandlers
+
+import (
+	"github.com/gordanet/gord/app/appmessage"
+	"github.com/gordanet/gord/app/rpc/rpccontext"
+	"github.com/gordanet/gord/infrastructure/network/netadapter/router"
+)
+
+// HandleNotifyVirtualDaaScoreChanged handles the respectively named RPC command
+func HandleNotifyVirtualDaaScoreChanged(context *rpccontext.Context, router *router.Router, _ appmessage.Message) (appmessage.Message, error) {
+	listener, err := context.NotificationManager.Listener(router)
+	if err != nil {
+		return nil, err
+	}
+	listener.PropagateVirtualDaaScoreChangedNotifications()
+
+	response := appmessage.NewNotifyVirtualDaaScoreChangedResponseMessage()
+	return response, nil
+}
